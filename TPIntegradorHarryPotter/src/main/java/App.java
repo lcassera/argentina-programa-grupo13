70316689e73ed99e72312b61ba8319550f4e04f8
pedro <prr@mail.com>
2023-04-24 21:57:34 -0300

    Modifico metodo procesarArchivoConValidacion en ProcesadorArchivoCsv.java;
    Modifico metodo main en App.java

--- TPIntegradorHarryPotter/src/main/java/App.java
+++ TPIntegradorHarryPotter/src/main/java/App.java
@@ -17,13 +17,22 @@ public class App {
         System.out.print("Ingrese el nombre del archivo de datos: ");
         String nombreArchivo = miEscanner.nextLine();
 
-        System.out.println("\nProcesando archivo...");
-
+        // Instanciamos la clase que procesará el archivo
         ProcesadorArchivoCsv procArchivo = new ProcesadorArchivoCsv(nombreArchivo);
-
-        ArrayList<Estudiante> lista = procArchivo.procesarArchivoConValidacion();
-
-       System.out.println("Proceso finalizado, " + lista.size() + " estudiantes leídos.");
+//        ArrayList<Estudiante> lista = procArchivo.procesarArchivo();
+        ArrayList<Estudiante> lista = null;
+        try {
+            System.out.println("\nPreparando el procesamiento del archivo...");
+            lista = procArchivo.procesarArchivoConValidacion();
+        } catch (FileNotFoundException e) {
+            System.out.println("No se pudo encontrar el archivo " + nombreArchivo.toString());
+            System.exit(1);
+        } catch (IOException e) {
+            System.out.println("No se pudo completar la lectura del archivo");
+            System.exit(-1);
+        }
+        // Mostrar al usuario que el archivo se procesó.
+        System.out.println("Proceso finalizado, " + lista.size() + " estudiantes leídos.");
 
        System.out.println("\nLista de Estudiantes:");
         System.out.println("---------------------------------");
